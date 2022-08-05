cc06731b4b3e5774e7d40bbb97418d67da7b909c
JesterCharles <JesterCharles@gmail.com>
2022-08-05 11:54:54 -0400

    updated the .gitignore to swap the out and target, added in junit for testing

--- src/main/java/com/revature/nabnak/daos/MemberDao.java
+++ src/main/java/com/revature/nabnak/daos/MemberDao.java
@@ -132,7 +132,7 @@ public class MemberDao implements Crudable<Member> {
 
             ResultSet rs = ps.executeQuery();
 
-            while(!rs.next()){
+            if(!rs.next()){
              throw new InvalidUserInputException("Enter information is incorrect for login, please try agian");
             }
 
